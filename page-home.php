04ddefee67fdcf46da71851bf25dbb56a596f6e0
Odara Amarakoon <lushanioamarakoon@gmail.com>
2023-07-06 16:25:44 +0530

    update 5sesctions

--- page-home.php
+++ page-home.php
@@ -85,9 +85,9 @@
                         </div>                     
                     </div>
                 <?php endwhile; ?>
-                <div class="link-btn">
-                    <a href="#"><?php getImage(get_field('service_link_image')); ?><?php the_field('service_link'); ?></a>
-                </div>
+                    <div class="link-btn">
+                        <a href="#"><?php getImage(get_field('service_link_image')); ?><?php the_field('service_link'); ?></a>
+                    </div>
 
 
                 </div>
@@ -95,16 +95,24 @@
     </section>
 <?php endif; ?>
 
-<!--Contact Us section -->
+<!--Contact Us  section -->
 <?php if (get_field('contact_content')) : ?>
     <section class="contact-section" id="welcome">
-    <?php getImage(get_field('contatct_us_image'), 'full-image'); ?>
+        <div class="contact-bground">
+        <?php getImage(get_field('contatct_us_image'), 'full-image'); ?>
+        </div>
+   
         <div class="container">
             <div class="row">
                 <div class="col-lg-6">
                     <div class="content-wrapper">
                         <?php the_field('contact_content'); ?>
+
+                        <div class="link-btn">
+                            <a href="#"><?php getImage(get_field('arrow_contact')); ?><?php the_field('contact_link'); ?></a>
+                        </div>
                     </div>
+                    
                 </div>           
             </div>
         </div>
@@ -112,74 +120,11 @@
 <?php endif; ?>
 
 
-<?php if (have_rows('packages')) : ?>
-    <section class="locations-section" id="our-prices">
-        <div class="container">
-            <?php if (get_field('packages_title')) : ?>
-                <div class="content-wrapper"><?php the_field('packages_title'); ?></div>
-            <?php endif; ?>
-            <?php if (have_rows('packages')) : ?>
-                <div class="row">
-                    <?php while (have_rows('packages')) : the_row(); ?>
-                        <div class="col-sm-12 col-md-6 col-lg-3">
-                            <div class="item">
-                                <div class="price"><span></span><?php the_sub_field('price'); ?></div>
-                                <h5><?php the_sub_field('title'); ?></h5>
-                                <?php if (get_sub_field('link')) : ?>
-                                    <a href="<?php the_sub_field('link'); ?>" target="_blank" class="theme-btn">
-                                        <?php the_field('packages_button_title'); ?>
-                                    </a>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                </div>
-            <?php endif; ?>
-        </div>
-    </section>
-<?php endif; ?>
 
-<?php if (get_field('contact_content')) : ?>
-    <section class="contact-section">
-        <?php getImage(get_field('contact_background_image'), 'full-image'); ?>
-        <div class="container">
-            <div class="content-wrapper">
-                <?php the_field('contact_content'); ?>
-                <?php if (get_field('contact_button_text')) : ?>
-                    <a href="#footer" class="theme-btn">
-                        <?php the_field('contact_button_text'); ?>
-                    </a>
-                <?php endif; ?>
-            </div>
-        </div>
-    </section>
-<?php endif; ?>
 
-<?php if (have_rows('testimonials')) : ?>
-    <section class="testimonials-section" id="what-they-say">
-        <div class="container">
-            <?php if (get_field('testimonials_title')) : ?>
-                <div class="content-wrapper"><?php the_field('testimonials_title'); ?></div>
-            <?php endif; ?>
-            <div class="swiper" id="testimonialsSwiper">
-                <div class="swiper-wrapper">
-                    <?php while (have_rows('testimonials')) : the_row(); ?>
-                        <div class="swiper-slide">
-                            <div class="item">
-                                <p><?php the_sub_field('testimonial'); ?></p>
-                                <h5><?php the_sub_field('name'); ?></h5>
-                            </div>
-                        </div>
-                    <?php endwhile; ?>
-                </div>
-                <div class="swiper-nav">
-                    <div class="swiper-btn testimonials-prev"><i class="fa-solid fa-chevron-left"></i></div>
-                    <div class="swiper-btn testimonials-next"><i class="fa-solid fa-chevron-right"></i></div>
-                </div>
-            </div>
-        </div>
-    </section>
-<?php endif; ?>
+
+
+
 
 <?php if (have_rows('areas')) : ?>
     <section class="areas-section">
